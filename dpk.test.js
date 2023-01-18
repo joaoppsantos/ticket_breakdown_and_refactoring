68d68a7e77cd30c87ba2e9f0ba7cc4e891062767
joaoppsantos <joaoppsantos@live.com>
2023-01-18 16:17:59 +0000

    test: Add unit tests for dpk file

--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,24 @@
 const { deterministicPartitionKey } = require("./dpk");
+const crypto = require("crypto");
 
 describe("deterministicPartitionKey", () => {
-  it("Returns the literal '0' when given no input", () => {
-    const trivialKey = deterministicPartitionKey();
-    expect(trivialKey).toBe("0");
+  it("should return 0 when event is not provided", () => {
+    expect(deterministicPartitionKey()).toEqual("0");
+  });
+
+  it("should return partitionKey when provided in event object", () => {
+    expect(
+      deterministicPartitionKey({ partitionKey: "mockPartitionKey" })
+    ).toEqual("mockPartitionKey");
+  });
+
+  it("should return hash of event object when partitionKey not provided", () => {
+    const event = { some: "data" };
+    const data = JSON.stringify(event);
+    const expectedHash = crypto
+      .createHash("sha3-512")
+      .update(data)
+      .digest("hex");
+    expect(deterministicPartitionKey(event)).toEqual(expectedHash);
   });
 });
